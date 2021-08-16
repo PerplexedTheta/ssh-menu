adb026f2e8c8fd7787da625a7931a81e9f50c339
?/θ <jacobdeery@msn.com>
2021-08-16 11:43:32 +0100

    Added scripts and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ssh-wrapper
 Just a quick and dirty login wrapper for an SSH gateway server 
+
+## Do this
+1.	Create two users - login and admin. Only admin should have sudo (passwordless if you use ssh keys) access.
+2.	Copy/clone the contents of bin to /usr/bin.
+3.	Chown the admin-shell.sh script to the admin user and chmod it to 0700.
+4.	Edit /etc/passwd and set the login shell of the login user to /usr/bin/sshwrapper.sh
+5.	Edit /etc/passwd and set the login shell of the admin user to /usr/bin/admin-shell.sh
+6. **Before** logging out, open a new ssh session and test both users. The admin should be able to access bash successfully with option 2.
+7. Done!
---
+++ bin/sshwrapper.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#
+# start keychain agent
+#
+eval `keychain --agents ssh --eval id_ed25519 --quiet`
+
+#
+# say hello
+#
+
+echo "[I]	Welcome to the Ishukone SSH gateway server!"
+echo "[W]	Unauthorised access is prohibited"
+
+#
+# get host
+#
+
+read -p "[I]	SSH server address: " host;
+
+if [ -z "$host" ]; then
+	echo "[E]	You failed to provide a hostname or IP address."
+	exit
+fi
+
+if [[ "$host" == "localhost" ]] || [[ "$host" == "0" ]] || [[ "$host" == "::" ]] || [[ "$host" == "::1" ]] || [[ "$host" == "127.0.0."* ]] || [[ "$host" == "0.0.0.0" ]] || [[ "$host" == "10."* ]] || [[ "$host" == "172.16."* ]] || [[ "$host" == "192.168."* ]]; then
+	echo "[E]	Connections to local machines cannot be performed without a valid hostname."
+	exit
+fi
+
+#
+# get port
+#
+
+read -p "[I]	SSH server port [22]: " port;
+
+if [ -z "$port" ]; then
+	port=22;
+fi
+
+if [[ -n ${port//[0-9]/} ]]; then
+	echo "[E]	The SSH server port must be a number between 0 and 65535."
+	exit
+fi
+
+#
+# get port
+#
+
+read -p "[I]	SSH server username [$USER]: " username;
+
+if [ -z "$username" ]; then
+	username=$USER;
+fi
+
+#
+# execute ssh command
+#
+exec ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o "LogLevel ERROR" -p $port $username@$host;
+exec exit;
