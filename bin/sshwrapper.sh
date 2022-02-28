0f4e863dd135dba9560e9e8cd43c22b04901c113
Jake Deery <jake.deery@ishukone.net>
2022-02-28 12:30:03 +0000

    Updated the scripts to be superior \o/

--- bin/sshwrapper.sh
+++ bin/sshwrapper.sh
@@ -1,62 +1,62 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#
-# start keychain agent
-#
+## find out if we're a sudoer
+sudo -n echo -n "" >/dev/null 2>&1
+isSudoer=$?
+
+
+## start keychain agent
 eval `keychain --agents ssh --eval id_ed25519 --quiet`
 
-#
-# say hello
-#
-echo "[I]	Welcome to the Ishukone SSH proxy server!"
+
+## say hello
 echo "[W]	Unauthorised access is prohibited"
-echo "[I]	To access this user's Bash terminal, use bash as the hostname below."
+if [[ "${isSudoer}" == "0" ]]; then
+	echo "[I]	Type 'bash' below to access a terminal session."
+fi
+echo "[I]	Type 'exit' below to quit this SSH session."
 
-#
-# get host
-#
-read -p "[I]	SSH server address: " host;
 
-if [ -z "$host" ]; then
+## get host
+read -p "[I]	SSH server address: " HOST;
+
+if [ -z "${HOST}" ]; then
 	echo "[E]	You failed to provide a hostname or IP address."
-	exit
+	exit 1
 fi
-
-if [[ "$host" == "localhost" ]] || [[ "$host" == "0" ]] || [[ "$host" == "::" ]] || [[ "$host" == "::1" ]] || [[ "$host" == "127.0.0."* ]] || [[ "$host" == "0.0.0.0" ]] || [[ "$host" == "10."* ]] || [[ "$host" == "172.16."* ]] || [[ "$host" == "192.168."* ]]; then
+if [[ "${HOST}" == "localhost" ]] || [[ "${HOST}" == "0" ]] || [[ "${HOST}" == "::" ]] || [[ "${HOST}" == "::1" ]] || [[ "${HOST}" == "127.0.0."* ]] || [[ "${HOST}" == "0.0.0.0" ]] || [[ "${HOST}" == "10."* ]] || [[ "${HOST}" == "172.16."* ]] || [[ "${HOST}" == "192.168."* ]]; then
 	echo "[E]	Connections to local machines cannot be performed without a valid hostname."
+	exit 1
+fi
+if [[ "${isSudoer}" == "0" && "${HOST}" == "bash" ]]; then
+	exec bash
+	exit 1
+fi
+if [[ "${HOST}" == "exit" ]]; then
 	exit
 fi
 
-if [[ "$host" == "bash" ]]; then
-	exec bash;
-	exec exit;
-fi
 
-#
-# get port
-#
-read -p "[I]	SSH server port [22]: " port;
+## get port
+read -p "[I]	SSH server port [22]: " PORT;
 
-if [ -z "$port" ]; then
+if [ -z "${PORT}" ]; then
 	port=22;
 fi
-
-if [[ -n ${port//[0-9]/} ]]; then
+if [[ -n ${PORT//[0-9]/} ]]; then
 	echo "[E]	The SSH server port must be a number between 0 and 65535."
 	exit
 fi
 
-#
-# get username
-#
-read -p "[I]	SSH server username [$USER]: " username;
+## get username
+read -p "[I]	SSH server username [$USER]: " USERNAME;
 
-if [ -z "$username" ]; then
+if [ -z "${USERNAME}" ]; then
 	username=$USER;
 fi
 
 #
 # execute ssh command
 #
-exec ssh -o "LogLevel ERROR" -F "${HOME}/.ssh/config" -p "$port" "$username@$host";
+exec ssh -o "LogLevel ERROR" -F "${HOME}/.ssh/config" -p "${PORT}" "${USERNAME}@${HOST}";
 exec exit;
