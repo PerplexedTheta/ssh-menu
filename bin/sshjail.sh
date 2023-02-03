360f19ae10035012e16f8f0c002dbf50fee71711
PerplexedTheta <jake.deery@ptfs-europe.com>
2023-02-03 16:29:34 +0000

    Update scripts to use dialog, not whiptail

--- bin/sshjail.sh
+++ bin/sshjail.sh
@@ -1,58 +1,99 @@
 #!/usr/bin/env bash
 
-
+##
 ## vars
-sudo -n echo -n "" >/dev/null 2>&1 # do not touch
-isSudoer=$? # set to 0 to enable bash for all users - $? is the default
+isSudoer=$(id -Gn | grep sudo) # set to 0 to enable bash for all users - $? is the default
 isOkay="" # do not touch
-hostsList=($(grep -P "^Host ([^*]+)$" ${HOME}/.ssh/config | sed 's/Host //' | sed ':a;N;$!ba;s/\n/ /g')) # do not touch
-tempArr='' # do not touch
-for (( i=0; i<${#hostsList[@]}; i++)); do
-	tempArr+=(${hostsList[$i]}) # whiptail likes a tag and description
-	tempArr+=(${hostsList[$i]}) # we just want one - duplicate them
-done
-if [[ "${isSudoer}" != "0"  ]]; then
-	continue
-else
-	tempArr+=("bash")
-	tempArr+=("bash")
-fi
-hostsList=(${tempArr[@]}) # do not touch
-host='' # do not touch
+title="Log on to "$(hostname) # change to edit menu titles
+showIntro="1" # set to 1 to show intro screen
+introMsg="Unauthorised access is prohibited." # change this to modify intro text
+userDataStr="" # create a string to hold the user-submitted form data
+host="" # set default hostname
+keyfilePath="~/.ssh/id_ed25519" # set default keyfile
+hostsList=($(cat ${HOME}/.ssh/config | grep -P "^Host ([^*]+)$" | sed 's/Host //' | sed ':a;N;$!ba;s/\n/ /g')) # do not touch
+username="${USER}" # set default username
 
 
+##
 ## start keychain agent
 eval $(keychain --agents ssh --eval id_ed25519 --quiet >/dev/null 2>&1) # run keychain if it exists
 
 
+##
 ## say hola
 clear
 echo "Performing interactive logon . . . "
-
-
-## what are we going to use - you decide
-if [[ "${isSudoer}" != "0"  ]]; then
-	host=$(whiptail --title "Log on to "$(hostname) --notags --menu "Please select a server from the list below:" 19 74 10 ${hostsList[@]} 3>&1 1>&2 2>&3) # ask for the hostname
-	isOkay=$?
-else
-	host=$(whiptail --title "Log on to "$(hostname) --notags --menu "Please select a server from the list below:\nTo access the local terminal, pick 'bash' instead:" 20 74 10 ${hostsList[@]} 3>&1 1>&2 2>&3) # ask for the hostname
+if [[ "${showIntro}" == "1" ]]; then
+	intro=$(/usr/bin/dialog --backtitle "${title}" --title "${title}" \
+	  --msgbox "${introMsg}" 25 65 \
+	  3>&1 1>&2 2>&3)
 	isOkay=$?
 fi
-if [[ -z "${host}" || "${host}" == "exit" || "${isOkay}" != "0" ]]; then
+
+
+##
+## what are we going to use - you decide
+if [[ "${isSudoer}" == ""  ]]; then
+	userData=$(/usr/bin/dialog --backtitle "${title}" --title "${title}" \
+	  --menu "Please select a server from the list below:" 25 65 19 \
+	  ${hostsList[@]} \
+	  3>&1 1>&2 2>&3)
+	isOkay=$?
+else
+	userDataStr=$(/usr/bin/dialog --backtitle "${title}" --title "${title}" \
+	  --extra-button --extra-label "Terminal" --menu "\n" 25 65 19 \
+	  ${hostsList[@]} \
+	  3>&1 1>&2 2>&3)
+	isOkay=$?
+fi
+if [[ -z "${userDataStr}" ]] || [[ "${isOkay}" == "1"  ]]; then
 	# silently exit here - obviously the user doesn't want to progress
 	exit 1
 fi
-if [[ "${host}" == "bash" ]]; then
-	if [[ "${isSudoer}" != "0" ]]; then
-		whiptail --msgbox "Your user is not permitted to access a terminal session. Bye!" 7 74 --title "${title}" 3>&1 1>&2 2>&3
+if [[ "${isOkay}" == "3"  ]]; then
+	# if the user ent a sudoer
+	if [[ "${isSudoer}" == "" ]]; then
+		# alert the user
+		errorText=$(/usr/bin/dialog --backtitle "${title}" --title "${title}" \
+		  --msgbox "E: ${USER} is not in the sudoers file. This incident will be reported." 25 65 \
+		  3>&1 1>&2 2>&3)
 		exit 1
 	else
-		exec bash
+		## if the user is a sudoer
+		exec /bin/bash
 		exit 0
 	fi
 fi
 
 
+## if the host element is unset
+if [[ "${userDataStr}" == "" ]]; then
+	# alert the user
+	errorText=$(/usr/bin/dialog --backtitle "${title}" --title "${title}" \
+	  --msgbox "E: You must enter a server address to connect via SSH" 25 65 \
+	  3>&1 1>&2 2>&3)
+	isOkay=1
+else
+	# set the var
+	host="${userDataStr}"
+fi
+## process isOkay
+if [[ "${isOkay}" != "0" ]]; then
+	# something has gone wrong
+	exit 1
+fi
+
+
+## check the values passed are valid ones
+if [[ "${host}" == "localhost" ]] || [[ "${host}" == "localhost.localnet"  ]] || [[ "${host}" == "0" ]] || [[ "${host}" == "::" ]] || [[ "${host}" == "::1" ]] || [[ "${host}" == "0.0.0.0"  ]] || [[ "${host}" == "127."* ]]; then
+	# alert the user
+	errorText=$(/usr/bin/dialog --backtitle "${title}" --title "${title}" \
+	  --msgbox "E: You cannot use a local address or hostname" 25 65 \
+	  3>&1 1>&2 2>&3)
+	exit 1
+fi
+
+
 ## execute ssh command
-exec ssh -o "LogLevel ERROR" -F "${HOME}/.ssh/config" "${host}"
+exec ssh -o "LogLevel ERROR" -F "/home/${USER}/.ssh/config" -i "${keyfilePath}" "${host}"
 exit 0
--- bin/sshwrapper.sh
+++ bin/sshwrapper.sh
@@ -1,98 +1,131 @@
 #!/usr/bin/env bash
 
-
+##
 ## vars
-sudo -n echo -n "" >/dev/null 2>&1 # do not touch
-isSudoer=$? # set to 0 to enable bash for all users - $? is the default
+isSudoer=$(id -Gn | grep sudo) # set to 0 to enable bash for all users - $? is the default
 isOkay="" # do not touch
 title="Log on to "$(hostname) # change to edit menu titles
-forceIntro="1" # set to 1 to show intro screen
+showIntro="1" # set to 1 to show intro screen
 introMsg="Unauthorised access is prohibited." # change this to modify intro text
-forceHost="0" # set to 1 to force a specific hostname
+userDataStr="" # create a string to hold the user-submitted form data
+userDataArr=() # create an array to hold the user-submitted form data
 host="" # set default hostname
-forcePort="0" # set to 1 to force a specific port
-port="22" # set default port
-forceUsername="0" # set to 1 to force a specific username
-username="${USER}" # dset default username
+keyfilePath="~/.ssh/id_ed25519" # set default keyfile
+username="${USER}" # set default username
 
 
+##
 ## start keychain agent
-eval $(keychain --agents ssh --eval id_ed25519 --quiet >/dev/null 2>&1)
+eval $(keychain --agents ssh --eval id_ed25519 --quiet >/dev/null 2>&1) # run keychain if it exists
 
 
+##
 ## say hola
 clear
 echo "Performing interactive logon . . . "
-if [[ "${forceIntro}" == "1" ]]; then
-	intro=$(whiptail --msgbox "${introMsg}" 7 74 --title "${title}" 3>&1 1>&2 2>&3)
+if [[ "${showIntro}" == "1" ]]; then
+	intro=$(/usr/bin/dialog --backtitle "${title}" --title "${title}" \
+	  --msgbox "${introMsg}" 25 65 \
+	  3>&1 1>&2 2>&3)
 	isOkay=$?
 fi
 
 
-## get host
-if [[ "${forceHost}" != "1" ]]; then
-	if [[ "${isSudoer}" != "0"  ]]; then
-		host=$(whiptail --inputbox "Please enter the hostname or IP of the server you wish to connect to below:" 9 74 "${host}" --title "${title}" 3>&1 1>&2 2>&3)
-		isOkay=$?
-	else
-		host=$(whiptail --inputbox "Please enter the hostname or IP of the server you wish to connect to below:\nTo access the local terminal, type 'bash' instead:" 10 74 "${host}" --title "${title}" 3>&1 1>&2 2>&3)
-		isOkay=$?
-	fi
+##
+## gather the data
+if [[ "${isSudoer}" == ""  ]]; then
+	userDataStr=$(/usr/bin/dialog --backtitle "${title}" --title "${title}" \
+	  --form "\n" 25 65 19 \
+	  "Username:"        1 1 "${USER}"                        1 17 40 40 \
+	  "Server address:"  2 1 "server.example.tld"             2 17 40 40 \
+	  "Keyfile path:"    3 1 "/home/${USER}/.ssh/id_ed25519"  3 17 40 40 \
+	  3>&1 1>&2 2>&3)
+	isOkay=$?
+else
+	userDataStr=$(/usr/bin/dialog --backtitle "${title}" --title "${title}" \
+	  --extra-button --extra-label "Terminal" --form "\n" 25 65 19 \
+	  "Username:"        1 1 "${USER}"                        1 17 40 40 \
+	  "Server address:"  2 1 "server.example.tld"             2 17 40 40 \
+	  "Keyfile path:"    3 1 "/home/${USER}/.ssh/id_ed25519"  3 17 40 40 \
+	  3>&1 1>&2 2>&3)
+	isOkay=$?
 fi
-if [[ -z "${host}" || "${host}" == "exit" || "${isOkay}" != "0" ]]; then
+if [[ -z "${userDataStr}" ]] || [[ "${isOkay}" == "1"  ]]; then
 	# silently exit here - obviously the user doesn't want to progress
 	exit 1
 fi
-if [[ "${host}" == "localhost" ]] || [[ "${host}" == "0" ]] || [[ "${host}" == "::" ]] || [[ "${host}" == "::1" ]] || [[ "${host}" == "127.0.0."* ]] || [[ "${host}" == "0.0.0.0" ]] || [[ "${host}" == "10."* ]] || [[ "${host}" == "172.16."* ]] || [[ "${host}" == "192.168."* ]]; then
-	whiptail --msgbox "You cannot use a local or private IP address or hostname. Bye!" 7 74 --title "${title}" 3>&1 1>&2 2>&3
-	exit 1
-fi
-if [[ "${host}" == "bash" ]] || [[ "${host}" == "zsh" ]] || [[ "${host}" == "fish" ]]; then
-	if [[ "${isSudoer}" != "0" ]]; then
-		whiptail --msgbox "Your user is not permitted to access a terminal session. Bye!" 7 74 --title "${title}" 3>&1 1>&2 2>&3
+if [[ "${isOkay}" == "3"  ]]; then
+	# if the user ent a sudoer
+	if [[ "${isSudoer}" == "" ]]; then
+		# alert the user
+		errorText=$(/usr/bin/dialog --backtitle "${title}" --title "${title}" \
+		  --msgbox "E: ${USER} is not in the sudoers file. This incident will be reported." 25 65 \
+		  3>&1 1>&2 2>&3)
 		exit 1
 	else
-		exec ${host}
+		## if the user is a sudoer
+		exec /bin/bash
 		exit 0
 	fi
 fi
 
 
-## get port
-if [[ "${forcePort}" != "1" ]]; then
-	port=$(whiptail --inputbox "Please enter the port of the server you wish to connect to below:" 8 74 ${port} --title "${title}" 3>&1 1>&2 2>&3)
-	isOkay=$?
-fi
-if [[ "${isOkay}" != "0" ]]; then
-	exit 1
-fi
-if [[ -z "${port}" ]]; then
-	whiptail --msgbox "You failed to provide a port number. Bye!" 7 74 --title "${title}" 3>&1 1>&2 2>&3
-	exit 1
-fi
-if [[ -n ${port//[0-9]/} ]]; then
-	whiptail --msgbox "You must provide a port between 1-65535. Bye!" 7 74 --title "${title}" 3>&1 1>&2 2>&3
-	exit 1
-fi
+##
+## process the data
+readarray -t userDataArr < <(printf '%s' "${userDataStr}")
 
 
-## get username
-if [[ "${forceUsername}" != "1" ]]; then
-	username=$(whiptail --inputbox "Please enter the username for the server you wish to connect to below:" 8 74 "${username}" --title "Log on to "$(hostname) 3>&1 1>&2 2>&3)
-	isOkay=$?
-fi
-if [[ "${isOkay}" != "0" ]]; then
-	exit 1
-fi
-
-
-## produce connection string
-if [[ "${username}" != "" ]]; then
-	conn="${username}@${host}"
+##
+## if the keyfile element is unset
+if [[ "${userDataArr[2]}" == "" ]]; then
+	# alert the user
+	errorText=$(/usr/bin/dialog --backtitle "${title}" --title "${title}" \
+	  --msgbox "E: You must enter a keyfile path to connect via SSH" 25 65 \
+	  3>&1 1>&2 2>&3)
+	isOkay=1
 else
-	conn="${host}"
+	# set the var
+	keyfilePath="${userDataArr[2]}"
 fi
+## if the host element is unset
+if [[ "${userDataArr[1]}" == "" ]]; then
+	# alert the user
+	errorText=$(/usr/bin/dialog --backtitle "${title}" --title "${title}" \
+	  --msgbox "E: You must enter a server address to connect via SSH" 25 65 \
+	  3>&1 1>&2 2>&3)
+	isOkay=1
+else
+	# set the var
+	host="${userDataArr[1]}"
+fi
+## if the user element is unset
+if [[ "${userDataArr[0]}" == "" ]]; then
+	# alert the user
+	errorText=$(/usr/bin/dialog --backtitle "${title}" --title "${title}" \
+	  --msgbox "E: You must enter a username to connect via SSH" 25 65 \
+	  3>&1 1>&2 2>&3)
+	isOkay=1
+else
+	# set the var
+	username="${userDataArr[0]}"
+fi
+## process isOkay
+if [[ "${isOkay}" != "0" ]]; then
+	# something has gone wrong
+	exit 1
+fi
+
+
+## check the values passed are valid ones
+if [[ "${host}" == "localhost" ]] || [[ "${host}" == "localhost.localnet"  ]] || [[ "${host}" == "0" ]] || [[ "${host}" == "::" ]] || [[ "${host}" == "::1" ]] || [[ "${host}" == "0.0.0.0"  ]] || [[ "${host}" == "127."* ]]; then
+	# alert the user
+	errorText=$(/usr/bin/dialog --backtitle "${title}" --title "${title}" \
+	  --msgbox "E: You cannot use a local address or hostname" 25 65 \
+	  3>&1 1>&2 2>&3)
+	exit 1
+fi
+
 
 ## execute ssh command
-exec ssh -o "LogLevel ERROR" -F "${HOME}/.ssh/config" -p "${port}" "${conn}"
+exec ssh -o "LogLevel ERROR" -F "/home/${USER}/.ssh/config" -i "${keyfilePath}" "${username}"@"${host}"
 exit 0
